f28220cfdf440384aa8be232e9568f5e22acde64
shubham1206agra <tt1191044@iitd.ac.in>
2022-01-27 18:12:49 +0530

    added profile

--- src/Freshers.js
+++ src/Freshers.js
@@ -29,5 +29,25 @@ export const FRESHERS = [
             
     
         ]
+    },
+    {
+        entryNum: 144,
+        name : "Shubham Agrawal",
+        image: '/GitAssignment2022/assets/cardImg.png',
+        branch: 'TT1',
+        about: 'Just another tech nerd',
+        contact: [
+            {
+                id: 2,
+                type: 'Github',
+                url: 'https://www.github.com/shubhamagra1206',
+            },
+            {
+                id: 3,
+                type: 'Facebook',
+                url: 'https://www.facebook.com/vansh.kachhwal.7',
+            }
+            
+        ]
     }
 ];
