2f0bbbfe8900d8abba3620b450ffaf38f0242b24
shubham1206agra <tt1191044@iitd.ac.in>
2022-01-27 12:30:45 +0530

    initial commit

---
+++ src/Freshers.js
@@ -0,0 +1,23 @@
+export const FRESHERS = [
+    {
+        entryNum: 143,
+        name : "Vansh Kachhwal",
+        image: '/assets/cardImg.png',
+        branch: 'MT1',
+        about: 'Just another technophile',
+        contact: [
+            {
+                id: 0,
+                type: 'instagram',
+                url: 'https://www.instagram.com/vk_230/'
+            }
+            ,{
+                id: 1,
+                type: 'LinkedIn',
+                url: 'https://www.linkedin.com/in/vansh-kachhwal-b638a8124/'
+            }
+            
+    
+        ]
+    }
+];
