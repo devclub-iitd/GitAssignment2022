c90af17314743592ba53312df9dac454d1ded5e8
Salil <salilgokhale24@gmail.com>
2022-01-28 08:23:21 +0530

    Added my code

---
+++ tests/Salil03_test.py
@@ -0,0 +1,16 @@
+print("Enter numbers in arithmetic progression:")
+a = input("Enter number 1: ")
+b = input("Enter number 2: ")
+c = input("Enter number 3: ")
+d = input("Enter number 4: ")
+e = input("Enter number 5: ")
+f = input("Enter number 6: ")
+g = input("Enter number 7: ")
+h = input("Enter number 8: ")
+i = input("Enter number 9: ")
+
+
+print("Magic square: ")
+print(h + " " + a + " " + f + " ")
+print(c + " " + e + " " + g + " ")
+print(d + " " + i + " " + b + " ")
