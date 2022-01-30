963601ad1bbe1282b413990b5213a59ee2cff7c8
Harsh <bb1200020@iitd.ac.in>
2022-01-30 20:50:52 +0530

    Made changes

--- src/Freshers.js
+++ src/Freshers.js
@@ -31,6 +31,27 @@ export const FRESHERS = [
         ]
     },
     {
+        entryNum: 20,
+        name : "Harsh Vardhan Singh Patel",
+        image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FHamster&psig=AOvVaw0qA60KzrmDNZ_8HLZEULBM&ust=1643642293833000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPjGrvDi2fUCFQAAAAAdAAAAABAD',
+        branch: 'BB1',
+        about: 'NVM',
+        contact: [
+            {
+                id: 2,
+                type: 'Github',
+                url: 'https://github.com/hvsp2704',
+            },
+            {
+                id: 3,
+                type: 'Instagram',
+                url: 'https://www.facebook.com/sa',
+            }
+            
+        ]
+    }
+
+    {
         entryNum: 144,
         name : "Shubham Agrawal",
         image: '/GitAssignment2022/assets/cardImg.png',
