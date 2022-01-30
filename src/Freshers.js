125e7225247dca38bfedf113e902e10886397106
Roshan Kumar <roshankumarredmi@gmail.com>
2022-01-31 00:47:04 +0530

    file is modified and image is added.

--- src/Freshers.js
+++ src/Freshers.js
@@ -1,53 +1,22 @@
-export const FRESHERS = [
-    {
-        entryNum: 143,
-        name : "Vansh Kachhwal",
-        image: '/GitAssignment2022/assets/cardImg.png',
-        branch: 'MT1',
-        about: 'Just another technophile',
-        contact: [
-            {
-                id: 0,
-                type: 'Instagram',
-                url: 'https://www.instagram.com/vk_230/'
-            },
-            {
-                id: 1,
-                type: 'LinkedIn',
-                url: 'https://www.linkedin.com/in/vansh-kachhwal-b638a8124/'
-            },
-            {
-                id: 2,
-                type: 'Github',
-                url: '',
-            },
-            {
-                id: 3,
-                type: 'Facebook',
-                url: 'https://www.facebook.com/vansh.kachhwal.7',
-            }
-            
-    
-        ]
-    },
-    {
-        entryNum: 144,
-        name : "Shubham Agrawal",
-        image: '/GitAssignment2022/assets/cardImg.png',
-        branch: 'TT1',
-        about: 'Just another tech nerd',
-        contact: [
-            {
-                id: 2,
-                type: 'Github',
-                url: 'https://www.github.com/shubhamagra1206',
-            },
-            {
-                id: 3,
-                type: 'Facebook',
-                url: 'https://www.facebook.com/sa',
-            }
-            
-        ]
-    }
-];
+export const FRESHERS = [{
+    entryNum: 181,
+    name: "Roshan Kumar",
+    image: '/GitAssignment2022/assets/2021EE30181.jpeg',
+    branch: 'EE3',
+    about: 'Just another technophile',
+    contact: [
+
+        {
+            id: 1,
+            type: 'LinkedIn',
+            url: 'https://www.linkedin.com/in/roshan-kumar-851561230/'
+        },
+        {
+            id: 2,
+            type: 'Github',
+            url: 'https://github.com/Ro-shanKumar',
+        },
+
+
+    ]
+}];
