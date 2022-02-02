e79837bae575bf883613ff8beae2d0855c982900
SATYAPRAGNYA-KAR <satyakar2019@gmail.com>
2022-02-02 21:44:27 +0530

    Git Bash Assignment Completed

--- src/Freshers.js
+++ src/Freshers.js
@@ -1,180 +1,206 @@
 export const FRESHERS = [
-    {
-        entryNum: 143,
-        name : "Vansh Kachhwal",
-        image: '/GitAssignment2022/assets/cardImg.png',
-        branch: 'MT1',
-        about: 'Just another technophile',
-        contact: [
-            {
-                id: 0,
-                type: 'Instagram',
-                url: 'https://www.instagram.com/vk_230/'
-            },
-            {
-                id: 1,
-                type: 'LinkedIn',
-                url: 'https://www.linkedin.com/in/vansh-kachhwal-b638a8124/'
-            },
-            {
-                id: 2,
-                type: 'Github',
-                url: '',
-            },
-            {
-                id: 3,
-                type: 'Facebook',
-                url: 'https://www.facebook.com/vansh.kachhwal.7',
-            }
-            
-    
-        ]
-    },
-    {
-        entryNum: 1044,
-        name : "Shubham Agrawal",
-        image: '/GitAssignment2022/assets/cardImg.png',
-        branch: 'TT1',
-        about: 'Just another tech nerd',
-        contact: [
-            {
-                id: 2,
-                type: 'Github',
-                url: 'https://www.github.com/shubhamagra1206',
-            },
-            {
-                id: 3,
-                type: 'Facebook',
-                url: 'https://www.facebook.com/sa',
-            }
-            
-        ]
-    },
-    {
-        entryNum: 905,
-        name : "Nancy Kansal",
-        image: 'https://i.pinimg.com/736x/77/cc/bb/77ccbbdda7411fe2c4feaf11a24a04e8--dear-daughter-girl-stuff.jpg',
-        branch: 'MT1',
-        about: 'Just another technophile',
-        contact: [
-            {
-                id: 0,
-                type: 'Github',
-                url: 'https://www.github.com/Nancy-Kansal-12',
-            }
-        ]
-    },
-    {
-        entryNum: 237,
-        name : "Salil Gokhale",
-        image: 'http://2.bp.blogspot.com/_sMQsqJIM_z0/TIOfp7F9w0I/AAAAAAAACVo/0hj-bLiKOz0/s1600/sphericalcow_430x300.jpg',
-        branch: 'MT1',
-        about: 'Just another random boi',
-        contact: [
-            {
-                id: 2,
-                type: 'Github',
-                url: 'https://github.com/Salil03',
-            }
-
-        ]
-    },
-    {
-        entryNum: 1139,
-        name : "Jaskaran Singh Bhalla",
-        image: '/GitAssignment2022/assets/2021TT11139.png',
-        branch: 'TT1',
-        about: 'Just another tech nerd',
-        contact: [
-            {
-                id: 2,
-                type: 'Instagram',
-                url: 'https://www.instagram.com/jaskaransinghbhalla/',
-            },
-            {
-                id: 3,
-                type: 'Facebook',
-                url: 'https://www.facebook.com/jaskaransinghbhalla/',
-            },
-            {
-                id: 4,
-                type: 'GitHub',
-                url: 'https://github.com/ultimatejaskaran',
-            }
-            
-        ]
-    },
-    {
-        entryNum: 1108,
-        name : "Shashank Mahawar",
-        image: 'https://shashank.biz/static/img/memoji2.png',
-        branch: 'ME2',
-        about: 'Perfectly Imperfect',
-        contact: [
-            {
-                id: 0,
-                type: 'Instagram',
-                url: 'https://www.instagram.com/shashmahawar/'
-            },
-            {
-                id: 1,
-                type: 'LinkedIn',
-                url: 'https://www.linkedin.com/in/shashmahawar/'
-            },
-            {
-                id: 2,
-                type: 'Github',
-                url: 'https://www.github.com/shashmahawar',
-            }
-            
-        ]
-    },
-    {
-        entryNum: 638,
-        name : "Yash Agarwal",
-        image: '/GitAssignment2022/assets/yash089610.jpeg',
-        branch: 'EE1',
-        about: 'Yet another noob, exploring the deep and dark secrets of LIFE',
-        contact: [
-            {
-                id: 0,
-                type: 'Facebook',
-                url: 'https://www.facebook.com/yash089610',
-            },
-            {
-                id: 1,
-                type: 'Instagram',
-                url: 'https://www.instagram.com/yash089610/'
-            },
-            {
-                id: 2,
-                type: 'LinkedIn',
-                url: 'https://www.linkedin.com/in/yash089610/'
-            },
-            {
-                id: 3,
-                type: 'Github',
-                url: 'https://github.com/Yash089610/',
-            },
-        ]
-    },
-    {
-        entryNum: 181,
-        name: "Roshan Kumar",
-        image: '/GitAssignment2022/assets/2021EE30181.jpeg',
-        branch: 'EE3',
-        about: 'Just another technophile',
-        contact: [
-            {
-                id: 1,
-                type: 'LinkedIn',
-                url: 'https://www.linkedin.com/in/roshan-kumar-851561230/'
-            },
-            {
-                id: 2,
-                type: 'Github',
-                url: 'https://github.com/Ro-shanKumar',
-            },
-        ]
-    },
+  {
+    entryNum: 143,
+    name: "Vansh Kachhwal",
+    image: "/GitAssignment2022/assets/cardImg.png",
+    branch: "MT1",
+    about: "Just another technophile",
+    contact: [
+      {
+        id: 0,
+        type: "Instagram",
+        url: "https://www.instagram.com/vk_230/",
+      },
+      {
+        id: 1,
+        type: "LinkedIn",
+        url: "https://www.linkedin.com/in/vansh-kachhwal-b638a8124/",
+      },
+      {
+        id: 2,
+        type: "Github",
+        url: "",
+      },
+      {
+        id: 3,
+        type: "Facebook",
+        url: "https://www.facebook.com/vansh.kachhwal.7",
+      },
+    ],
+  },
+  {
+    entryNum: 1044,
+    name: "Shubham Agrawal",
+    image: "/GitAssignment2022/assets/cardImg.png",
+    branch: "TT1",
+    about: "Just another tech nerd",
+    contact: [
+      {
+        id: 2,
+        type: "Github",
+        url: "https://www.github.com/shubhamagra1206",
+      },
+      {
+        id: 3,
+        type: "Facebook",
+        url: "https://www.facebook.com/sa",
+      },
+    ],
+  },
+  {
+    entryNum: 905,
+    name: "Nancy Kansal",
+    image:
+      "https://i.pinimg.com/736x/77/cc/bb/77ccbbdda7411fe2c4feaf11a24a04e8--dear-daughter-girl-stuff.jpg",
+    branch: "MT1",
+    about: "Just another technophile",
+    contact: [
+      {
+        id: 0,
+        type: "Github",
+        url: "https://www.github.com/Nancy-Kansal-12",
+      },
+    ],
+  },
+  {
+    entryNum: 237,
+    name: "Salil Gokhale",
+    image:
+      "http://2.bp.blogspot.com/_sMQsqJIM_z0/TIOfp7F9w0I/AAAAAAAACVo/0hj-bLiKOz0/s1600/sphericalcow_430x300.jpg",
+    branch: "MT1",
+    about: "Just another random boi",
+    contact: [
+      {
+        id: 2,
+        type: "Github",
+        url: "https://github.com/Salil03",
+      },
+    ],
+  },
+  {
+    entryNum: 1139,
+    name: "Jaskaran Singh Bhalla",
+    image: "/GitAssignment2022/assets/2021TT11139.png",
+    branch: "TT1",
+    about: "Just another tech nerd",
+    contact: [
+      {
+        id: 2,
+        type: "Instagram",
+        url: "https://www.instagram.com/jaskaransinghbhalla/",
+      },
+      {
+        id: 3,
+        type: "Facebook",
+        url: "https://www.facebook.com/jaskaransinghbhalla/",
+      },
+      {
+        id: 4,
+        type: "GitHub",
+        url: "https://github.com/ultimatejaskaran",
+      },
+    ],
+  },
+  {
+    entryNum: 1108,
+    name: "Shashank Mahawar",
+    image: "https://shashank.biz/static/img/memoji2.png",
+    branch: "ME2",
+    about: "Perfectly Imperfect",
+    contact: [
+      {
+        id: 0,
+        type: "Instagram",
+        url: "https://www.instagram.com/shashmahawar/",
+      },
+      {
+        id: 1,
+        type: "LinkedIn",
+        url: "https://www.linkedin.com/in/shashmahawar/",
+      },
+      {
+        id: 2,
+        type: "Github",
+        url: "https://www.github.com/shashmahawar",
+      },
+    ],
+  },
+  {
+    entryNum: 638,
+    name: "Yash Agarwal",
+    image: "/GitAssignment2022/assets/yash089610.jpeg",
+    branch: "EE1",
+    about: "Yet another noob, exploring the deep and dark secrets of LIFE",
+    contact: [
+      {
+        id: 0,
+        type: "Facebook",
+        url: "https://www.facebook.com/yash089610",
+      },
+      {
+        id: 1,
+        type: "Instagram",
+        url: "https://www.instagram.com/yash089610/",
+      },
+      {
+        id: 2,
+        type: "LinkedIn",
+        url: "https://www.linkedin.com/in/yash089610/",
+      },
+      {
+        id: 3,
+        type: "Github",
+        url: "https://github.com/Yash089610/",
+      },
+    ],
+  },
+  {
+    entryNum: 181,
+    name: "Roshan Kumar",
+    image: "/GitAssignment2022/assets/2021EE30181.jpeg",
+    branch: "EE3",
+    about: "Just another technophile",
+    contact: [
+      {
+        id: 1,
+        type: "LinkedIn",
+        url: "https://www.linkedin.com/in/roshan-kumar-851561230/",
+      },
+      {
+        id: 2,
+        type: "Github",
+        url: "https://github.com/Ro-shanKumar",
+      },
+    ],
+  },
+  {
+    entryNum: 767,
+    name: "Satyapragnya Kar",
+    image:
+      "https://i.pinimg.com/564x/4d/83/3e/4d833e81203ae6f3a76e92913175c3a9.jpg",
+    branch: "MS1",
+    about: "A Hopeless Nincompoop",
+    contact: [
+      {
+        id: 0,
+        type: "Facebook",
+        url: "https://www.facebook.com/profile.php?id=100072259100482",
+      },
+      {
+        id: 1,
+        type: "Instagram",
+        url: "https://www.instagram.com/satyapragnya_kar/",
+      },
+      {
+        id: 2,
+        type: "LinkedIn",
+        url: "https://www.linkedin.com/in/satyapragnya-kar-118b3221b/",
+      },
+      {
+        id: 3,
+        type: "Github",
+        url: "https://github.com/SATYAPRAGNYA-KAR",
+      },
+    ],
+  },
 ];
