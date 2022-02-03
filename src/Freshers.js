3b0d8c1991192ceae442d94fdc8eceeea5d82064
Shiven Rathore <shivenapr4@gmail.com>
2022-02-04 02:59:15 +0530

    Added my details in Freshers.js

--- src/Freshers.js
+++ src/Freshers.js
@@ -1,7 +1,7 @@
 export const FRESHERS = [
     {
         entryNum: 143,
-        name : "Vansh Kachhwal",
+        name: "Vansh Kachhwal",
         image: '/GitAssignment2022/assets/cardImg.png',
         branch: 'MT1',
         about: 'Just another technophile',
@@ -26,13 +26,13 @@ export const FRESHERS = [
                 type: 'Facebook',
                 url: 'https://www.facebook.com/vansh.kachhwal.7',
             }
-            
-    
+
+
         ]
     },
     {
         entryNum: 144,
-        name : "Shubham Agrawal",
+        name: "Shubham Agrawal",
         image: '/GitAssignment2022/assets/cardImg.png',
         branch: 'TT1',
         about: 'Just another tech nerd',
@@ -47,7 +47,31 @@ export const FRESHERS = [
                 type: 'Facebook',
                 url: 'https://www.facebook.com/sa',
             }
-            
+
+        ]
+    },
+    {
+        entryNum: 327,
+        name: "Shiven Rathore",
+        image: 'https://img1.looper.com/img/gallery/detective-pikachu-2-release-date-cast-and-plot/intro-1568566852.jpg',
+        branch: 'BB',
+        about: 'really interested for devclub',
+        contacts: [
+            {
+                id: 0,
+                type: 'GitHub',
+                url: 'https://github.com/shivarr1',
+            },
+            {
+                id: 1,
+                type: 'Instagram',
+                url: 'https://www.instagram.com/shiven.rathore/',
+            },
+            {
+                id: 2,
+                type: 'Facebook',
+                url: 'https://www.facebook.com/profile.php?id=100076626291571',
+            }
         ]
     }
 ];
