ea739ee390e2d9ffa055b344bca53f901f053f41
Khushvind Maurya <mauryakhusvind@gmail.com>
2022-02-04 18:05:48 +0530

    added python test

--- tests/khushvind_test.py
+++ tests/khushvind_test.py
@@ -3,23 +3,18 @@ def inc(x):
 
 def test_answer():
     assert inc(3) == 4
-    assert inc(36) == 37
-
 
 def sum(x,y):
     return (x+y)
 
 def test_sum():
     assert sum(4,6) == 10
-    assert sum(7,6) == 11
-
-
+    
 def len(P):
     return len(P)
 
 def test_len():
     assert len([1,2,3,4,5,6,7]) == 7
-    assert len([1,2,3,4,5,7]) == 6
 
 
 def power(x,y):
@@ -27,7 +22,6 @@ def power(x,y):
 
 def test_power():
     assert power(2,3) == 8
-    assert power(3,4) == 81
 
 
 def remainder(a,b):
@@ -35,5 +29,4 @@ def remainder(a,b):
 
 def test_remainder():
     assert remainder(7,3) == 1
-    assert remainder(7,2) == 1
-    assert remainder(10,2) == 0
+ 
