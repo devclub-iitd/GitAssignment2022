a43537029bab625a3b5e55f4354573f2ca2fec89
Vatsal Varshney <vatsalvar1211@gmail.com>
2022-02-04 18:37:19 +0530

    modified python code

--- tests/vatsalvarshney_test.py
+++ tests/vatsalvarshney_test.py
@@ -5,7 +5,7 @@ def listsum(l1,l2):
     return l
 
 def test_1():
-    assert listsum([8,3,4,0],[2,4,6,1])==[10,7,10,1]
+    assert listsum([8,3,4,0],[2,4,6,1]) == [10,7,10,1]
 
 def test_2():
-    assert listsum([9,4,8],[3,7,3])==[12,11,11]
+    assert listsum([9,4,8],[3,7,3]) == [12,11,11]
