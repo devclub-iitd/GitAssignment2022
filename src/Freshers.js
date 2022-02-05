099f51fe8290d7d51ac063cb326e6d1eb728295f
sidg2110 <siddharth.gupta.2110@gmail.com>
2022-02-05 20:28:13 +0530

    Edited by Siddharth Gupta

--- src/Freshers.js
+++ src/Freshers.js
@@ -177,4 +177,28 @@ export const FRESHERS = [
             },
         ]
     },
-];
+    {
+        entryNum: 627,
+        name: "Siddharth Gupta",
+        image: '/GitAssignment2022/assets/2021EE1210627.jpeg',
+        branch: 'EE1',
+        about: 'Just another technophile',
+        contact: [
+            {
+                id: 1,
+                type: 'Instagram',
+                url: 'https://www.instagram.com/siddharth.2110/',
+            },
+            {
+                id: 2,
+                type: 'GitHub',
+                url: 'https://github.com/sidg2110'
+            }
+            {
+                id: 3,
+                type: 'Facebook',
+                url: 'https://www.facebook.com/profile.php?id=100073791277157',
+            },
+        ]
+    },
+    
