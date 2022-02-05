25e0e9a0c6c40bd91e92e98c1dc8eddeb88b6f42
Basil Labib <basillabib01@gmail.com>
2022-02-06 00:47:28 +0530

    Fix: assertionError

--- tests/basil08_test.py
+++ tests/basil08_test.py
@@ -4,4 +4,4 @@ def test_len():
   assert len([x**2 for x in range(100)]) == 100
 
 def test_sum():
-  assert sum([2*x-1 for x in range(1, 99)]) ==  99**2
+  assert sum([2*x-1 for x in range(1, 99)]) ==  98**2
