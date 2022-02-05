e5998a1f64f6dbb04533205c9b74ff9a45765ce4
Ashaeyx <96980688+Ashaeyx@users.noreply.github.com>
2022-02-05 15:27:04 +0530

    Update Ashaeyx_test.py

--- tests/Ashaeyx_test.py
+++ tests/Ashaeyx_test.py
@@ -1,22 +1,34 @@
+""" Rock Paper Scissors
+----------------------------------------
+"""
 import random
-import turtle
-colors = ['red','cyan','pink' ,'yellow', 'green','orange']
-t = turtle.Turtle()
-t.speed(10)
-turtle.bgcolor("black")
-length=100
-angle =50
-size=5
-for i in range(length):
-    color=random.choice(colors)
-    t.pencolor(color)
-    t.fillcolor(color)
-    t.penup()
-    t.forward(i+50)
-    t.pendown()
-    t.left(angle)
-    t.begin_fill()
-    t.circle(size)
-    t.end_fill()
-turtle.exitonclick()
-turtle.bgcolor("black") 
+import os
+import re
+os.system('cls' if os.name=='nt' else 'clear')
+while (1 < 2):
+    print "\n"
+    print "Rock, Paper, Scissors - Shoot!"
+    userChoice = raw_input("Choose your weapon [R]ock], [P]aper, or [S]cissors: ")
+    if not re.match("[SsRrPp]", userChoice):
+        print "Please choose a letter:"
+        print "[R]ock, [S]cissors or [P]aper."
+        continue
+    // Echo the user's choice
+    print "You chose: " + userChoice
+    choices = ['R', 'P', 'S']
+    opponenetChoice = random.choice(choices)
+    print "I chose: " + opponenetChoice
+    if opponenetChoice == str.upper(userChoice):
+        print "Tie! "
+    #if opponenetChoice == str("R") and str.upper(userChoice) == "P"
+    elif opponenetChoice == 'R' and userChoice.upper() == 'S':      
+        print "Scissors beats rock, I win! "
+        continue
+    elif opponenetChoice == 'S' and userChoice.upper() == 'P':      
+        print "Scissors beats paper! I win! "
+        continue
+    elif opponenetChoice == 'P' and userChoice.upper() == 'R':      
+        print "Paper beat rock, I win! "
+        continue
+    else:       
+        print "You win!"
