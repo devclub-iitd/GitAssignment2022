5e3337695fc36297d826ace98bdbaae0f3d38427
Esha Patel <esha7679@gmail.com>
2022-02-05 23:05:07 +0530

    edited

--- tests/Eshapatel701_test.py
+++ tests/Eshapatel701_test.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-def test_len():
-    assert len([1,2,3,4]) == 4
+def test():
+    assert len([1,2,3,4,5,6]) == 6
 
 def test_numpy():
-    assert np.sum([1,2,3,4]) == 10
+    assert np.sum([1,2,3,4,5,6]) == 22
