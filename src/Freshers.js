adccd76d322f42f0e0135666b527395c37be7132
Jyotiraditya <yjyotiraditya@gmail.com>
2022-02-05 21:38:09 +0530

    did change

--- src/Freshers.js
+++ src/Freshers.js
@@ -439,18 +439,24 @@ export const FRESHERS = [
     },
     {
         entryNum: 96,
-        name: "Jyotiraditya Yadav"
-        branch: "CS1"
-        image: "https://www.jagranimages.com/images/newimg/05072021/05_07_2021-03_07_2020-funny__jokes_in_hindi_20468806_114715988_21801133.jpg"
-        about:"Some Assignments are cool ,some"
+        name: "Jyotiraditya Yadav",
+        branch: 'CS1',
+        image: 'https://www.jagranimages.com/images/newimg/05072021/05_07_2021-03_07_2020-funny__jokes_in_hindi_20468806_114715988_21801133.jpg',
+        about:'Some Assignments are cool ,some' , 
         contact:[
            {  id: 0,
-            type: "Instagram",
-            url:"https://www.instagram.com/jyotiradityadav/"
+            type: 'Instagram',
+            url:'https://www.instagram.com/jyotiradityadav/'
            },{
                id: 1 ,
-               type: "Github",
-               url: "https://github.com/Sigmale"
+               type: 'Github',
+               url: 'https://github.com/Sigmale'
+           }
+           ,{
+               id: 2,
+               type: 'LinkedIn' ,
+               url:'linkedin.com/in/jyotiraditya-yadav-7060a720b'
+
            }
         ]
     }
