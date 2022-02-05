7bb9063e7d271dd340e6442f84a13e4b1781e730
[adil imran] <[emraanadil07@gmail.com]>
2022-02-05 13:22:30 +0530

    input sanitization

--- tests/emraanadil_test.py
+++ tests/emraanadil_test.py
@@ -1,10 +1,18 @@
 
 def count_down(start_number):
-  current=start_number
-  while (current > 0):
-    print(current)
-    current -= 1
-  print("Zero!")
+    
 
-start_number=int(input("Countdown from: "))
-count_down(start_number)
+    current=start_number
+    while (current > 0):
+        print(current)
+        current -= 1
+    print("Zero!")
+
+while True:
+  try:
+    num = int(input("Enter an integer number: "))
+    break
+  except ValueError:
+      print("Please input integer only...")  
+      continue
+count_down(num)
