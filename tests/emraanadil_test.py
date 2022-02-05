0fc3a4dada530624b43a419d1a95a500e62e6194
[adil imran] <[emraanadil07@gmail.com]>
2022-02-05 16:12:53 +0530

    non interactive

--- tests/emraanadil_test.py
+++ tests/emraanadil_test.py
@@ -8,11 +8,5 @@ def count_down(start_number):
         current -= 1
     print("Zero!")
 
-while True:
-  try:
-    num = int(input("Enter an integer number: "))
-    break
-  except ValueError:
-      print("Please input integer only...")  
-      continue
+num=11
 count_down(num)
