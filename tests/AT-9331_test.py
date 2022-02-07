22cd6fb91de25c1fd3c2ec6398841f2c53fe83c3
AT-9331 <71870002+AT-9331@users.noreply.github.com>
2022-02-07 23:18:57 +0530

    Update AT-9331_test.py

--- tests/AT-9331_test.py
+++ tests/AT-9331_test.py
@@ -25,22 +25,4 @@ for a in range(m):
     print(fx(n,a)) 
     print('')
 
-while True:   
-    fd=input('Is The Test Working? <y/n>: ')
-    if fd=='y':
-        print('ok')
-        print('....~....')
-        break
-
-    if fd=='n':
-        for a in range(5):
-            print(' '*a + '*'*a)
-        print('....~....')
-        break  
-
-    if fd=='exit':
-        print('....~....')
-        break
-
-    else:
-        print('enter <y/n> (or) exit')  
+ 
