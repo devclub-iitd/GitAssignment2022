f64b12394bdf72bbd1c467c6cc1372e6cd846dc1
2nav <n-raj@outlook.com>
2022-03-11 00:15:50 +0530

    Added entry 240

--- src/Freshers.js
+++ src/Freshers.js
@@ -190,7 +190,7 @@ export const FRESHERS = [
         ]
     },
     {
-      {
+      
         entryNum: '2021BB10321',
         name : "Dhruv Jain",
         image: '/GitAssignment2022/public/assets/Spiderman.jpg',
